186e90026d396cabe5733118172d5d1a04bc18d6
TupleSosok <jokylionplay@gmail.com>
2024-03-20 19:30:22 +0300

    Шаблоны методов

--- src/main/java/org/conteiner/IntArray.java
+++ src/main/java/org/conteiner/IntArray.java
@@ -12,6 +12,10 @@ public class IntArray {
      * @value начальная длинна при создании
      */
     static private final int DEFAULT_LEN = 10;
+    /**
+     * @value значение на сколько увеличиваться масив при переполнении
+     */
+    static private final int INCREMENT_STEP = 10;
 
     /**
      * Текущая длинна
@@ -31,5 +35,32 @@ public class IntArray {
         length = 0;
     }
 
+    /**
+     * Add. Добавление в массив
+     * Если переполнен должен создаваться с новой большей длинной
+     * @param num the num
+     */
+    public void add(int num){
+
+    }
+
+    /**
+     * Delete.
+     *
+     * @param index the index
+     */
+    public void delete(int index){
+
+    }
+
+    /**
+     * Int at int.
+     *
+     * @param index the index
+     * @return the int
+     */
+    public int intAt(int index){
+        return 0;
+    }
 
 }
