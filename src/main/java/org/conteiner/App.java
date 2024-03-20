f92638d44c8c5fa7d9b5567cc687239d2d11f695
TupleSosok <jokylionplay@gmail.com>
2024-03-20 19:22:50 +0300

    Создание класса IntArray констукртор + поля

--- src/main/java/org/conteiner/App.java
+++ src/main/java/org/conteiner/App.java
@@ -1,16 +1,19 @@
 package org.conteiner;
 
+
 /**
- * Hello world!
- *
+ * The type App.
  */
-public class App 
+public class App
 {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
-
-
 }
